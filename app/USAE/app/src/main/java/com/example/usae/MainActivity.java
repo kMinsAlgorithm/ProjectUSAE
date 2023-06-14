1eaec42936c9bf0a2a4a33d5fc6ba5fc168f0b33
k013600 <k012123600@gmail.com>
2023-06-14 22:16:20 +0900

    feat: USAE 어플 기능 구현 완료 및 주석 추가

--- app/USAE/app/src/main/java/com/example/usae/MainActivity.java
+++ app/USAE/app/src/main/java/com/example/usae/MainActivity.java
@@ -7,9 +7,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//처음 실행대기 화면에서 실행 가능 상태 화면으로 넘어가기 위해 작성한 코드.
 public class MainActivity extends AppCompatActivity {
 
-    private static int DELAY_TIME = 3000; // 3초
+    private static int DELAY_TIME = 3000; // 3초 이 코드는 로봇과 휴대폰이 연결되는 그 사이 기간을 표현하기 위해서 작성해 둔건데 나중에 보고서 쓸때는 포함안키시는게 좋을듯
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/USAE/app/src/main/java/com/example/usae/SecondActivity.java
+++ app/USAE/app/src/main/java/com/example/usae/SecondActivity.java
@@ -8,13 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
         import android.app.DatePickerDialog;
         import android.content.Intent;
         import android.content.pm.PackageManager;
-        import android.os.Build;
+import android.os.AsyncTask;
+import android.os.Build;
         import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognitionListener;
         import android.speech.RecognizerIntent;
         import android.speech.SpeechRecognizer;
-        import android.view.View;
+import android.util.Log;
+import android.view.View;
         import android.widget.Button;
         import android.widget.DatePicker;
         import android.widget.EditText;
@@ -25,7 +27,10 @@ import android.speech.RecognitionListener;
         import android.widget.TextView;
         import android.widget.Toast;
 
-        import java.text.SimpleDateFormat;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
         import java.util.ArrayList;
         import java.util.Calendar;
         import java.util.Date;
@@ -40,8 +45,10 @@ public class SecondActivity extends AppCompatActivity {
     boolean recording = false;  //현재 녹음중인지 여부
     TextView textView;
     ImageButton recordButton;
-    EditText contentsTextView;	//음성을 텍스트로 변환한 결과를 출력할 텍스트뷰
-
+    Socket socket;
+    PrintWriter writer;
+    // 전송할 정수형 변수
+    int destination;
     private static int DELAY_TIME = 3000; // 3초
 
     @Override
@@ -73,6 +80,7 @@ public class SecondActivity extends AppCompatActivity {
         //버튼 클릭 이벤트 리스터 등록
     }
 
+    //녹음 시작 버튼이 눌린다면
     View.OnClickListener click = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -94,6 +102,7 @@ public class SecondActivity extends AppCompatActivity {
     };
 
     RecognitionListener listener = new RecognitionListener() {
+        //비어 있는 함수 구문은 보고서에도 생략해도 되고 이해하는 것도 생략해도 될듯
         @Override
         public void onReadyForSpeech(Bundle bundle) {
 
@@ -121,6 +130,7 @@ public class SecondActivity extends AppCompatActivity {
         }
 
         @Override
+        // 음성 인식 도중 문제가 발생할 경우 예외 처리 정의 (예외 처리에 대한 단어는 검색해보는게 좋을거야)
         public void onError(int error) {    //토스트 메세지로 에러 출력
             String message;
             switch (error) {
@@ -131,7 +141,7 @@ public class SecondActivity extends AppCompatActivity {
                     //message = "클라이언트 에러";
                     //speechRecognizer.stopListening()을 호출하면 발생하는 에러
                     return; //토스트 메세지 출력 X
-                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS: //어플을 처음 실행했을때 사진 접근, 파일 접근 ,마이크 접근 권한 확인시 거절 누르면 예외 처리 발생
                     message = "퍼미션 없음";
                     break;
                 case SpeechRecognizer.ERROR_NETWORK:
@@ -169,27 +179,43 @@ public class SecondActivity extends AppCompatActivity {
         //인식 결과가 준비되면 호출
         @Override
         public void onResults(Bundle bundle) {
+            //음성 인식을 하여 matches에 음성 인식 내용을 저장함.
             ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);	//인식 결과를 담은 ArrayList
-//            String originText = contentsTextView.getText().toString();  //기존 text
             Toast.makeText(getApplicationContext(), "변환중", Toast.LENGTH_SHORT).show();
-            //인식 결과
+
+            //음성 인식 내용의 자료형이 arraylist인데 이를 string으로 변환시켜주는 과정
             String newText="";
             for (int i = 0; i < matches.size() ; i++) {
                 newText += matches.get(i);
             }
-
+            //인식 받은 내용중에 우리가 원하는 단어가 있는지 찾아서 명령을 인식하는 방식
             if(newText.contains("일") || newText.contains("1")) {
                 textView.setText("1번 위치로 이동합니다.");
+                destination = 1;
+                //이 코드들은 인식한 명령을 로봇에게 전달하기 위한 코드.
+                BackGroundTask b1 = new BackGroundTask();
+                b1.execute();
+//                destination=0;
 //                Toast.makeText(getApplicationContext(), "1번 위치로 이동합니다.", Toast.LENGTH_SHORT).show();
             } else if (newText.contains("이") || newText.contains("2")) {
                 textView.setText("2번 위치로 이동합니다.");
+                destination = 2;
+                BackGroundTask b2 = new BackGroundTask();
+                b2.execute();
+//                destination=0;
 //                Toast.makeText(getApplicationContext(), "2번 위치로 이동합니다.", Toast.LENGTH_SHORT).show();
             } else if(newText.contains("삼") || newText.contains("3")) {
                 textView.setText("3번 위치로 이동합니다.");
-//                Toast.makeText(getApplicationContext(), "3번 위치로 이동합니다.", Toast.LENGTH_SHORT).show();
-            }
+                destination=3;
+                BackGroundTask b3 = new BackGroundTask();
+                b3.execute();
+            } else if(newText.contains("원위치") || newText.contains("홈")) {
+                textView.setText("원래 위치로 이동합니다.");
+                destination=4;
+                BackGroundTask b5 = new BackGroundTask();
+                b5.execute();
+        }
 
-//            contentsTextView.setText(originText + newText + " ");	//기존의 text에 인식 결과를 이어붙임
             speechRecognizer.startListening(intent);    //녹음버튼을 누를 때까지 계속 녹음해야 하므로 녹음 재개
         }
 
@@ -234,11 +260,43 @@ public class SecondActivity extends AppCompatActivity {
     //녹음 중지
     void StopRecord() {
         recording = false;
-
+        destination=0;
+        // 녹은 중지 버튼을 눌렀을때 로봇을 정지시키게 하기 위해서 정시 신호를 담은 통신을 보냄.
+        BackGroundTask b4 = new BackGroundTask(); // 정지하기 위해 통신을 버튼을 눌러 음성 인식을 종료할때도 소켓 통신을 보냄
+        b4.execute();
         //마이크 이미지와 텍스트 변경
         textView.setText("USAE가 준비되었습니다\n버튼을 눌러주세요");
         recordButton.setImageResource(R.drawable.activate_button);
         speechRecognizer.stopListening();   //녹음 중지
 //        Toast.makeText(getApplicationContext(), "음성 기록을 중지합니다.", Toast.LENGTH_SHORT).show();
     }
+
+    // 소켓 통신 기능이 있는 곳.
+    class BackGroundTask extends AsyncTask<Void, Void, Void> {
+        Handler handler = new Handler();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                if (socket == null || socket.isClosed()) {
+                    // IP 주소와 포트 번호에 맞게 수정
+                    socket = new Socket("172.20.10.10", 6000);
+                    writer = new PrintWriter(socket.getOutputStream());
+                    Log.i("i", "CONNECTED");
+                }
+                // destination을 전역 변수로 지정해 두었는데, 음성 명령을 하게 되면 어픙에서 명령을 필터링 해서 해당 명령을 의미하는 숫자를 desination에 저장하도록 구현함.
+                //1: 1번 위치 , 2: 2번 위치, 3: 3번 위치, 4: 원위치, 0: 종료 신호
+                //그래서 전달하고자 하는 메시지에 destination이라는 변수를 담아
+                String message = String.valueOf(destination);
+                //전송
+                writer.write(message);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
 }
